a27c7400f3ae5154f2f2cd3db5cf1f213fb83332
MolecularDust <30814968+MolecularDust@users.noreply.github.com>
2018-12-09 01:27:19 +0300

    Help comments edit

--- CrossFader/CrossFader.cs
+++ CrossFader/CrossFader.cs
@@ -1,11 +1,13 @@
 ﻿/*
+    This is a mass crossfade script for Vegas Pro that allows to create and remove crossfades and video transitions and change their properties.
+
     Basic operation: set crossfade's length in milliseconds or frames and click OK button.
     This will create crossfadеs in place of cuts for every selected event.
 
     "Allow Loops": by default the script skips events that have loops, i.e. the event's right margin exceeds the original clip length.
     Nor does it create a crossfade if detects that looping occurs when making a crossfade. 
     This is made to avoid rogue cuts in video and "broken record" stutters in audio. 
-    Loops are ok, however, for generated media like text or solids.
+    Loops are okay, however, for generated media like text or solids.
     Check this box to skip loop detection and always create a crossfade in a selected event no matter what.
 
     If you wish you may add a Transition to the newly created crossfades in video events.
@@ -16,7 +18,7 @@
 
     "Do Nothing With Crossfades" skips crossfades module completely and allows creating or removing transitions in video events solely.
 
-    The script can operate in non-intecactive mode, without GUI.
+    The script can operate in non-interactive mode, without GUI.
     To do so change script's settings below and set Settings.ShowDialog to false and tune other settings to your needs.
     This way you can copy the script to a new file with custom settings and run it triggered by a shortcut key.
 
